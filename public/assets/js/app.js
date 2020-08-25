7c12caa456f91446ddb4542df659b41e5d79c7ef
塚原彩 <tsukahara@seez.jp>
2020-08-25 10:30:02 +0900

    日付ごとにまとめる

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -93,8 +93,26 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-var SLACK_CLIENT_TOKEN = 'xoxp-1330090420449-1316092925989-1341363678864-f9e50cd2696119a1969b520d68c26c8c';
+var SLACK_CLIENT_TOKEN = 'xoxp-1330090420449-1316092925989-1312311013990-b86b0f855dcc8b84635700cf0c45a884';
 var conversationId = 'G019A44CK53'; // チャンネルのID
+// fetch('https://slack.com/api/conversations.history',{
+//   method:'GET',
+//   mode:'cors',
+//   headers: {
+//     'token':SLACK_CLIENT_TOKEN,
+//     'channel':conversationId,
+//     'Content-type':'application/x-www-form-urlencoded'
+//   }
+// })
+// .then(response => {
+//   return response.json();
+// })
+// .then(json => {
+//   console.log(json);
+// });
+// .catch(reason => {
+//   console.log(reason);
+// });
 
 $.ajax({
   url: 'https://slack.com/api/conversations.history',
@@ -105,10 +123,28 @@ $.ajax({
     channel: conversationId
   },
   timeout: 5000
-}).done(function (data) {
-  console.log(data);
-}).fail(function () {
-  console.log(data);
+}).then(function (response) {
+  if (response.ok) {
+    var result = [];
+    Object.entries(response.messages).forEach(function (item) {
+      var unixtime = item[1].ts;
+      var datetime = new Date(unixtime * 1000);
+      var year = datetime.getFullYear();
+      var month = datetime.getMonth() + 1;
+      var date = datetime.getDate();
+      var postdate = "".concat(year, "/").concat(month, "/").concat(date);
+
+      if (!result[postdate]) {
+        result[postdate] = item;
+      } else {
+        var indent = result[postdate].length;
+        result[postdate][indent] = item;
+      }
+    });
+    console.log(result);
+  }
+})["catch"](function () {
+  console.log('error');
 });
 
 /***/ })
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,25 @@
 
-const SLACK_CLIENT_TOKEN = 'xoxp-1330090420449-1316092925989-1341363678864-f9e50cd2696119a1969b520d68c26c8c';
+const SLACK_CLIENT_TOKEN = 'xoxp-1330090420449-1316092925989-1312311013990-b86b0f855dcc8b84635700cf0c45a884';
 const conversationId = 'G019A44CK53' // チャンネルのID
 
+// fetch('https://slack.com/api/conversations.history',{
+//   method:'GET',
+//   mode:'cors',
+//   headers: {
+//     'token':SLACK_CLIENT_TOKEN,
+//     'channel':conversationId,
+//     'Content-type':'application/x-www-form-urlencoded'
+//   }
+// })
+// .then(response => {
+//   return response.json();
+// })
+// .then(json => {
+//   console.log(json);
+// });
+// .catch(reason => {
+//   console.log(reason);
+// });
 $.ajax({
   url: 'https://slack.com/api/conversations.history',
   type: 'GET',
@@ -12,9 +30,27 @@ $.ajax({
   },
   timeout: 5000,
 })
-.done(function(data) {
-  console.log(data);
+.then(function(response) {
+  if(response.ok) {
+  let result = [];
+  Object.entries(response.messages).forEach((item)=>{
+    const unixtime = item[1].ts;
+    const datetime = new Date(unixtime*1000);
+    const year = datetime.getFullYear();
+    const month = datetime.getMonth() + 1;
+    const date = datetime.getDate();
+    const postdate = `${year}/${month}/${date}`;
+    if(!result[postdate]) {
+      result[postdate] = item;
+    } else {
+      const indent = result[postdate].length;
+      result[postdate][indent] = item;
+    }
+
+  });
+    console.log(result);
+  }
 })
-.fail(function() {
-  console.log(data);
+.catch(function() {
+  console.log('error');
 });
